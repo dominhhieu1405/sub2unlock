76e80706ee036b3eac2004659bff4a37eef90727
dominhhieu1405 <51281387+dominhhieu1405@users.noreply.github.com>
2020-03-23 20:02:02 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Sub2Unlock - Link Shortener Service</title>
+        <meta name="description" content="Link Shortening service that helps creators reach their goals!">
+        <meta name="author" content="Miro">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.sub2unlock.com/css/main.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" />
+        <link rel="shortcut icon" href="https://cdn.sub2unlock.com/images/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="https://cdn.sub2unlock.com/images/favicon.ico" type="image/x-icon" />
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,700" rel="stylesheet">
+        <style>
+            #header.row {
+                width: 100%;
+                max-width: 100% !important;
+                margin: 0 auto;
+            }
+        </style>
+    </head>
+    <body id="top">
+        <header id="header" class="row">
+            <div class="header-logo">
+                <a href="#">
+                    <img src="cdn/images/Title.png" alt="Sub2Unlock Logo">
+                </a>
+            </div>
+            <nav id="header-nav-wrap">
+                <ul class="header-main-nav">
+                    <li>
+                        <a class="smoothscroll" href="#about" title="about">About</a>
+                    </li>
+                    <li>
+                        <a class="smoothscroll" href="#testimonials" title="testimonials1">Reviews</a>
+                    </li>
+                    <li>
+                        <a
+                              class="smoothscroll"
+                              href="https://sub2unlock.com/auth/login"
+                              title="Login"
+                              onclick="window.location.href = 'https://sub2unlock.com/auth/login'"
+                        >Login</a>
+                    </li>
+                </ul>
+                <a href="https://sub2unlock.com/auth/register" class="button1 button1-primary cta">Sign Up</a>
+            </nav>
+            <a class="header-menu-toggle" href="#">
+                <span>Menu</span>
+            </a>
+        </header>
+        <section id="home" data-parallax="scroll" data-natural-width="3000" data-natural-height="2000">
+            <div class="overlay"></div>
+            <div class="home-content">
+                <div class="row contents">
+                    <div class="home-content-left">
+                        <h3 data-aos="fade-up">Welcome to Sub2Unlock</h3>
+                        <h1 data-aos="fade-up">
+                            A Service Dedicated to helping creators reach their true potential!
+                        </h1>
+                        <div class="grid">
+                            <a href="https://sub2unlock.com/auth/register" class="smoothscroll button1 stroke" onClick="document.location.href = 'https://sub2unlock.com/auth/register'">
+                                <span class="fas fa-address-card" aria-hidden="true" id="contact-icon"></span> Sign Up
+                            </a>
+                            <a href="https://sub2unlock.com/auth/login" class="smoothscroll button1 stroke" id="mobileShow" onClick="document.location.href = 'https://sub2unlock.com/auth/login'">
+                                <span class="fas fa-sign-in-alt" aria-hidden="true" id="log-icon"></span> Login
+                            </a>
+                            <a href="https://www.youtube.com/embed/2hey3CpjMcA" data-lity class="button1 stroke">
+                                <span class="fas fa-play" aria-hidden="true"></span> Watch Tutorial
+                            </a>
+                            <!--<p id="paragraphvasty"><a href="https://vasty.cc" id="vasty1"><b>Or create links without signing up</b></a> </p> -->
+                        </div>
+                    </div>
+                    <div class="home-image-right">
+                        <img src="cdn/images/both.png" srcset="cdn/images/both.png 1x, https://cdn.sub2unlock.com/images/both.png 2x" data-aos="fade-up" class="aos-init aos-animate" alt="Desktop & Mobile mockup">
+                    </div>
+                </div>
+            </div>
+            <ul class="home-social-list">
+                <li>
+                    <a href="http://vk.com/share.php?url=#" target="_blank">
+                        <i class="fab fa-vk" aria-hidden="true" id="vk"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.facebook.com/share.php?u=#/&amp;title=#" target="_blank">
+                        <i class="fab fa-facebook-f" id="facebook"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://twitter.com/home?status=Sub2Unlock+#" target="_blank">
+                        <i class="fab fa-twitter" id="twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" target="_blank">
+                        <i class="fab fa-discord" id="discord"></i>
+                    </a>
+                </li>
+            </ul>
+            <div class="home-scrolldown">
+                <a href="#about" class="scroll-icon smoothscroll">
+                    <span>Scroll Down</span>
+                    <i class="fas fa-arrow-right" aria-hidden="true"></i>
+                </a>
+            </div>
+        </section>
+        <section id="about">
+            <div class="row about-intro">
+                <div class="col-four">
+                    <h1 class="intro-header" data-aos="fade-up">About Our Service</h1>
+                </div>
+                <div class="col-eight">
+                    <p class="lead" data-aos="fade-up">
+                        Sub2Unlock is a service dedicated to helping content creators reach their true potential! Sub2Unlock allows content creators to link all their social media profiles in one page and set a target for their followers! watch the video above for a tutorial on how to get started with Sub2Unlock.com
+                    </p>
+                </div>
+            </div>
+            <div class="row about-features">
+                <div class="features-list block-1-3 block-m-1-2 block-mob-full group">
+                    <div class="bgrid feature" data-aos="fade-up">
+                                <span class="icon">
+                            <i class="fab fa-windows"></i>
+                            </span>
+                        <div class="service-content">
+                            <h3>Fully Resposive</h3>
+                            <p>Are you tired of unfully responsive sites? Sites that might work on a 480p screen and doesn't work on a 720p screen or sites that work on pc and don't work on Mobile. We're sure you're very tired of that and so, we have designed our site to be fully responsive on all devices. No exceptions! Unless you can come up with really weird dimensions of course.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="bgrid feature" data-aos="fade-up">
+                                <span class="icon">
+                            <i class="fas fa-desktop"></i>
+                            </span>
+                        <div class="service-content">
+                            <h3>Retina Ready</h3>
+                            <p> Our site sure is responsive for the most commonly used screens but it doesn't end there! Our site is Retina ready meaning that even users with retina displays that require higher resolution images will be able to enjoy the site. In fact they will probably enjoy the site even more since they will be browsing it at a much higher resolution! :D
+                            </p>
+                        </div>
+                    </div>
+                    <div class="bgrid feature" data-aos="fade-up">
+                                <span class="icon">
+                            <i class="fas fa-brush"></i>
+                            </span>
+                        <div class="service-content">
+                            <h3>Stylish Design</h3>
+                            <p>Just being fully responsive is not enough if the site has ugly aesthetics right? We know you all hate bad aesthetics just as much as we do, so we hired professional designers and color scheme experts to make this site as good looking and user friendly as possible. And we can tell you that that was a huge success!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="bgrid feature" data-aos="fade-up">
+                                <span class="icon">
+                            <i class="fas fa-code"></i>
+                            </span>
+                        <div class="service-content">
+                            <h3>Clean Code</h3>
+                            <p>Aren't you tired of poorly coded sites that take up to 20 SECONDS of your life to load? Well say goodbye to that because with our clean code and our cutting edge server technology our average load time is lower than 1 second all over the world! We can not reveal our server structure for obvious reasons but you can just imagine how complicated it would be to have world wide support ;)
+                            </p>
+                        </div>
+                    </div>
+                    <div class="bgrid feature" data-aos="fade-up">
+                                <span class="icon">
+                            <i class="fas fa-sliders-h"></i>
+                            </span>
+                        <div class="service-content">
+                            <h3>Easy To Customize</h3>
+                            <p>What's the point of having such a great service if you can't cuztomize it? Our service is already super customizable and with every new update it is becoming even more customizable. You can use your own link text, unlock button text, and soon even the page layout and color scheme! This gives you a lot of power. So Use It Responsibly and enjoy it! :)
+                            </p>
+                        </div>
+                    </div>
+                    <div class="bgrid feature" data-aos="fade-up">
+                                <span class="icon">
+                            <i class="fas fa-gift"></i>
+                            </span>
+                        <div class="service-content">
+                            <h3>Free of Charge</h3>
+                            <p>Isn't it very disappointing when you finally find the service you've always been looking for then discover that it's paid? Maybe you're a student or don't have an online payment solution or you just don't want to spend money on the service just yet. And we totally understand that! with your emotional and financial support we were able to keep this service free!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row about-how">
+                <h1 class="intro-header" data-aos="fade-up">How The Service Works?</h1>
+                <div class="about-how-content" data-aos="fade-up">
+                    <div class="about-how-steps block-1-2 block-tab-full group">
+                        <div class="bgrid step" data-item="1">
+                            <h3>Sign Up</h3>
+                            <p>Sign Up now! It's completely free and it's really all your need to start. All you need is an email!
+                            </p>
+                        </div>
+                        <div class="bgrid step" data-item="2">
+                            <h3>Login</h3>
+                            <p>Login to your dashboard. This is where you will be able to create your social media links and check their statistics.
+                            </p>
+                        </div>
+                        <div class="bgrid step" data-item="3">
+                            <h3>Create Your Link</h3>
+                            <p>Once you're in your dashboard click on create, add the social media links you want to add and click Generate!
+                            </p>
+                        </div>
+                        <div class="bgrid step" data-item="4">
+                            <h3>Share and enjoy!</h3>
+                            <p>Now that you have created your Sub To Unlock link you are ready to share it with the world and enjoy! :D
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="testimonials">
+            <div class="row">
+                <div class="col-twelve">
+                    <h1 class="intro-header" data-aos="fade-up">What They Say About Our Service.</h1>
+                </div>
+            </div>
+            <div class="row owl-wrap">
+                <div id="testimonial-slider" data-aos="fade-up">
+                    <div class="slides owl-carousel">
+                        <div>
+                            <p>
+                                Sub2Unlock is a brilliant tool, my fans want to use services and download links and Sub2Unlock helps me out to grow and get love and its well Constructed and isnt like a survey i really like Sub2Unlock to become a tool most YouTubers should use to grow it helps alot Thanks!!!
+                            </p>
+                            <div class="testimonial-author">
+                                <img src="cdn/images/avatars/user-02.jpg" alt="Author image">
+                                <div class="author-info">
+                                    Alfie
+                                    <span class="position">Sub2Unlock User</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <p>
+                                Sub2Unlock's simple interface has made my life way easier and now i can generate links that help me grow with a click of a button.
+                            </p>
+                            <div class="testimonial-author">
+                                <img src="cdn/images/avatars/user-03.jpg" alt="Author image">
+                                <div class="author-info">
+                                    Elia
+                                    <span>Sub2Unlock User</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer>
+            <div class="footer-main">
+                <div class="row">
+                    <div class="col-five md-1-5 tab-full footer-info">
+                        <div class="footer-logo">
+                            <img src="cdn/images/Title.png" alt="Sub2Unlock Logo">
+                        </div>
+                        <p>© 2018 Miro Services</p>
+                        <p>
+                            <a href="http:cdn/images/dmca-badge-w150-5x1-01.png" alt="DMCA.com Protection Status">
+                            </a>
+                        </p>
+                    </div>
+                    <div class="col-three md-1-3 tab-1-2 mob-full footer-site-links">
+                        <h4>Site Links</h4>
+                        <ul class="list-links">
+                            <li>
+                                <a href="#">Advertise here</a>
+                            </li>
+                            <li>
+                                <a href="/privacy">Privacy Policy</a>
+                            </li>
+                            <li>
+                                <a href="#" target="_blank">Support</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-four md-1-4 tab-1-2 mob-full footer-contact">
+                        <h4>Follow Us Here</h4>
+                        <p>
+                            <a href="http://vk.com/share.php?url=#" target="_blank" id="vk_box">
+                                <i class="fab fa-vk" aria-hidden="true" id="vk2"></i>
+                            </a>
+                            <a href="http://www.facebook.com/share.php?u=#&amp;title=Sub2Unlock" target="_blank" id="facebook_box">
+                                <i class="fab fa-facebook-f" id="facebook2"></i>
+                            </a>
+                            <a href="http://twitter.com/home?status=Sub2Unlock+#/" target="_blank" id="twitter_box">
+                                <i class="fab fa-twitter" id="twitter2"></i>
+                            </a>
+                            <a href="#" target="_blank" id="discord_box">
+                                <i class="fab fa-discord" id="discord2"></i>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <div id="preloader">
+        </div>
+        </script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="cdn/js/frontend/modernizr.min.js"></script>
+        <script src="cdn/js/frontend/pace.min.js"></script>
+        <script src="cdn/js/frontend/plugins.min.js"></script>
+        <script src="cdn/js/frontend/main.min.js"></script>
+    </body>
+</html>
